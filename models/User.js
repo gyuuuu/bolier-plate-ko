0cf59c680c955152b16765786f2766a360659572
gyu <kai9702@gmail.com>
2020-08-18 21:41:49 +0900

    로그인 기능 구현_ user.comparePassword() 에러

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -53,6 +54,31 @@ userSchema.pre('save', function( next ){
         next()
     }
 })
+
+userSchema.method.comparePassword = function(plainPassword, cb){
+    // ex) plainPassword =  1234567    암호화된 비번 : $2b$10$VK/CCfJjaZanIQsO0AR40eRlckLa0ExNJD48X28SqVN8mc5ulzOoy
+    // plainPassword를 암호화해서 DB에 암호화된 비번과 같은 지 확인
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err); //비밀번화가 같지 않다면
+        cb(null, isMatch);
+    })
+}
+
+userSchema.method.generateToken = function(cb) {
+
+    var user = this;
+
+    // jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    //user._id + 'secretToken' = token
+
+    user.token = token //db의 token필드에 생성한 token 넣어주기
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
