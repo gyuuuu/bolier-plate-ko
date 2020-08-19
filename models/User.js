ce095dedbb33402161310f34bc4b707b8e2c4836
gyu <kai9702@gmail.com>
2020-08-19 17:54:34 +0900

    Auth  기능 추가.

--- models/User.js
+++ models/User.js
@@ -80,6 +80,21 @@ userSchema.methods.generateToken = function(cb) {
     })
 }
 
+userSchema.statics.findByToken = function(token, cb){
+    var user = this;
+
+    //토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // decode하면 유저 아이디가 나온다 -> decoded는 유저 아이디
+        // 유저 아이디를 이용해서 유저를 찾은 다음 
+        // 클라이언트에서 가져온 토큰과 DB에 저장된 토큰이 일치하는지 확인
+        user.findOne({"_id": decoded, "token": token}, function(err, user) {
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
