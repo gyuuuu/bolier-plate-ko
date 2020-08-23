e73a52dbf81a8568dd16553d968c41f52f5e0ec8
gyu <kai9702@gmail.com>
2020-08-23 16:39:54 +0900

    server-client간 데이터 통신 에러 (CORS이슈) 해결-Proxy설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ function LandingPage() {
 
     // Landing 페이지에 들어오자마자 get req보냄
     useEffect(() => {
-        axios.get('http://localhost:5000/api/hello') //end point : api/hello
+        axios.get('/api/hello') //end point : api/hello
         .then(response => console.log(response.data)) // 그후 서버로부터 돌아오는 res를 콘솔에 보여줌
     }, [])
 
