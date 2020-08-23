557126e00f4e2d0b2776ab02fb33fd49e0508e0b
gyu <kai9702@gmail.com>
2020-08-23 16:26:32 +0900

    server와 client간의 데이터 통신-axios사용

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect} from 'react'
+import axios from 'axios'
 
 function LandingPage() {
+
+    // Landing 페이지에 들어오자마자 get req보냄
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/hello') //end point : api/hello
+        .then(response => console.log(response.data)) // 그후 서버로부터 돌아오는 res를 콘솔에 보여줌
+    }, [])
+
     return (
         <div>
             LandingPage
--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요~ hi~')
 })
 
+app.get('/api/hello', (req, res) => {
+  res.send("안녕하세요~~")
+})
+
 
 app.post('/api/users/register', (req, res) => {
   //회원가입 할 때 필요한 정보들을 client에서 가져오면
