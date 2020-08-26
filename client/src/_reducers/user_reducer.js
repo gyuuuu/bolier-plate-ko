d6735fabf888ab781d64b3b19098573aaf14f20b
gyu <kai9702@gmail.com>
2020-08-26 16:01:02 +0900

    회원가입 페이지 완료-REDUX 이용

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,5 +1,6 @@
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from '../_actions/types';
 
 
@@ -9,6 +10,10 @@ export default function (state = {}, action){
             return { ...state, loginSuccess: action.payload }
             break;
         
+        case  REGISTER_USER:
+            return { ...state, register: action.payload}
+            break;
+
         default:
             return state;
     }
